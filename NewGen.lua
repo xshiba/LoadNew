6ddad168ec80e6e26c55cc359dc7cf189d4ffbac
xshiba <92438125+xshiba@users.noreply.github.com>
2022-11-23 20:14:02 +0700

    Update NewGen.lua

--- NewGen.lua
+++ NewGen.lua
@@ -13,9 +13,9 @@ spawn(
                         if tostring(d[2]) ~= "true" and tostring(d[2]) ~= "false" then
                             if UseSkill then
                                 if PosMonMasteryFruit ~= nil and SaveSettings["Main"]["AutoFarmFruitMastery"] then
-                                    if tostring(typeof(args[2])) == "CFrame" then
+                                    if tostring(typeof(d[2])) == "CFrame" then
                                         d[2] = PosMonMasteryFruit
-                                    elseif tostring(typeof(args[2])) == "Vector3" then
+                                    elseif tostring(typeof(d[2])) == "Vector3" then
                                         d[2] = PosMonMasteryFruit.Position
                                     end
                                     return b(unpack(d))
@@ -44,9 +44,9 @@ spawn(
                         if tostring(d[2]) ~= "true" and tostring(d[2]) ~= "false" then
                             if USEGUN then
                                 if PosMonMasteryGun ~= nil and SaveSettings["Main"]["AutoFarmGunMastery"] then
-                                    if tostring(typeof(args[2])) == "CFrame" then
+                                    if tostring(typeof(d[2])) == "CFrame" then
                                         d[2] = PosMonMasteryGun
-                                    elseif tostring(typeof(args[2])) == "Vector3" then
+                                    elseif tostring(typeof(d[2])) == "Vector3" then
                                         d[2] = PosMonMasteryGun.Position
                                     end
                                     return b(unpack(d))
@@ -74,9 +74,9 @@ spawn(
                     if tostring(d[1]) == "RemoteEvent" then
                         if tostring(d[2]) ~= "true" and tostring(d[2]) ~= "false" then
                             if SaveSettings["Main"]["AutoSeaBeast"] and FindSeabeast then
-                                if tostring(typeof(args[2])) == "CFrame" then
+                                if tostring(typeof(d[2])) == "CFrame" then
                                     d[2] = SeaBeastPos
-                                elseif tostring(typeof(args[2])) == "Vector3" then
+                                elseif tostring(typeof(d[2])) == "Vector3" then
                                     d[2] = SeaBeastPos.Position
                                 end
                                 return b(unpack(d))
@@ -103,9 +103,9 @@ spawn(
                     if tostring(d[1]) == "RemoteEvent" then
                         if tostring(d[2]) ~= "true" and tostring(d[2]) ~= "false" then
                             if SaveSettings["Combat"]["Aimbot_Skill_Around"] then
-                                if tostring(typeof(args[2])) == "CFrame" then
+                                if tostring(typeof(d[2])) == "CFrame" then
                                     d[2] = AimbotNearestSelectPosition
-                                elseif tostring(typeof(args[2])) == "Vector3" then
+                                elseif tostring(typeof(d[2])) == "Vector3" then
                                     d[2] = AimbotNearestSelectPosition.Position
                                 end
                                 return b(unpack(d))
@@ -135,9 +135,9 @@ spawn(
                                 SaveSettings["Combat"]["FovEnabled"] and
                                     (SaveSettings["Combat"]["Aimbot_Gun"] or SaveSettings["Combat"]["Aimbot_Skill"])
                              then
-                                if tostring(typeof(args[2])) == "CFrame" then
+                                if tostring(typeof(d[2])) == "CFrame" then
                                     d[2] = _G.CharacterAimBot.Character.HumanoidRootPart.CFrame
-                                elseif tostring(typeof(args[2])) == "Vector3" then
+                                elseif tostring(typeof(d[2])) == "Vector3" then
                                     d[2] = _G.CharacterAimBot.Character.HumanoidRootPart.Position
                                 end
                                 return b(unpack(d))
@@ -164,9 +164,9 @@ spawn(
                     if tostring(d[1]) == "RemoteEvent" then
                         if tostring(d[2]) ~= "true" and tostring(d[2]) ~= "false" then
                             if SaveSettings["Main"]["DFMasteryCake"] and MagnetDoughtDF then
-                                if tostring(typeof(args[2])) == "CFrame" then
+                                if tostring(typeof(d[2])) == "CFrame" then
                                     d[2] = PosMonDoughtOpenDoorDF
-                                elseif tostring(typeof(args[2])) == "Vector3" then
+                                elseif tostring(typeof(d[2])) == "Vector3" then
                                     d[2] = PosMonDoughtOpenDoorDF.Position
                                 end
                                 return b(unpack(d))
